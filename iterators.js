ef9b131b5378bfd832bcb705724d9749463460c2
sparksys98 <hanineiroukh@gmail.com>
2020-03-08 11:32:18 +0200

    Task 3 Done

--- iterators.js
+++ iterators.js
@@ -5,7 +5,7 @@
  * - Logs every element of the array
  ************************************/
 const logger = function(array) {
-  // Your code here
+array.forEach(city => console.log(city));
 };
 
 /*************************************
@@ -20,7 +20,7 @@ const logger = function(array) {
  *   C = (F - 32) * (5/9)
  ************************************/
 const toCelsius = function(temperatures) {
-  // Your code here
+ return temperatures.map(arr => arr=(arr-32)*(5/9));
 };
 
 /**************************************
@@ -32,7 +32,7 @@ const toCelsius = function(temperatures) {
  *   that exceed the threshhold
  ***************************************/
 const hottestDays = function(temperatures, threshhold) {
-  // Your code here
+  return temperatures.filter(arr => arr > threshhold);
 };
 
 /******************************************
@@ -50,7 +50,7 @@ const hottestDays = function(temperatures, threshhold) {
  *       all previous functions
  *******************************************/
 const logHottestDays = function(temperatures, threshhold) {
-  // Your code here
+ logger(toCelsius(hottestDays(temperatures, threshhold)));
 };
 
 export { logger, toCelsius, hottestDays, logHottestDays };
